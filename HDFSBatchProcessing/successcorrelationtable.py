845d2feefa589160f823c97fa79a0e9eec66f36b
barshashrest <shres22b@mtholyoke.edu>
2015-07-10 18:19:52 -0700

    Update successcorrelationtable.py

--- HDFSBatchProcessing/successcorrelationtable.py
+++ HDFSBatchProcessing/successcorrelationtable.py
@@ -43,6 +43,7 @@ tweets = sqlContext.sql("SELECT created_at, id, id_str, text, favorite_count, re
 nowplayingmovies = sqlContext.sql("SELECT original_title, release_date, popularity, vote_average, date FROM nowplaying")
 nowplayingmovies.registerTempTable("nowplayingmovies")
 
+#get date for the API. Date has been added into the JSON API of TMDB manually and needs to be extracted.
 dateRDD = sqlContext.sql("select date from nowplayingmovies where date!='null'")
 datelist = dateRDD.collect()
 dateonly=''
@@ -57,16 +58,17 @@ for d in datelist:
 	month=int(dateonly[5:7])
 	day = int(dateonly[8:10])
 	
-#collectionofmovieresults = nowplayingmovies.collect()
 
 
 
+#get all movies in the list
 movienames = nowplayingmovies.map(lambda p: p.original_title)
-#tweets.groupBy("created_at").count().show()
-#tag
 
-#movie_list = movienames.collect()
+
+#collect all the movie names
 movie_list = movienames.collect()
+
+#add the date column into the TMDB database table
 def add_date(row):
 		date_added = dateonly
 		year_added = year
@@ -74,10 +76,13 @@ def add_date(row):
 		day_added = day
 		return(row.asDict()["original_title"], row.asDict()["release_date"], row.asDict()["popularity"], row.asDict()["vote_average"], date_added, year_added, month_added, day_added )
 
+#final RDD with now playing movies and date
 nowplaying_with_date = nowplayingmovies.map(add_date).coalesce(40)
 
+#dataframe for nowplaying and upcoming movies
 NPdf = sqlContext.createDataFrame(nowplaying_with_date, ['original_title', 'release_date', 'popularity', 'voteaverage', 'date_added', 'year_added', 'month_added', 'day_added'])
 
+#add movie name as column to the tweets database so as to query for only the tweets with the movie name
 def add_movie_col(row):
 	movie_name='null'
 	for movie in movie_list:
@@ -90,22 +95,22 @@ def add_movie_col(row):
 				row.asDict()["text"] = ''
 	return(row.asDict()["created_at"], row.asDict()["text"], movie_name)
 
-	
+#add the date to the rdd of tweets	
 for movie in movie_list:
 	tweet_with_movie_rdd = tweets.map(add_movie_col).coalesce(40)
 	
-#tweet_with_movie_and_date_rdd = tweets.map(add_movie_col)
 
+#create dataframe with the following columns
 tweet_with_movie_df = sqlContext.createDataFrame(tweet_with_movie_rdd, ['created_at', 'text', 'movie_name'])         
-#tweet_with_movie_df.filter(tweet_with_movie_df['movie_name']!='Dope').take(10)
 
+# filter from table such that all the remaining tweets are tweets explicity about the movies in the movielist 
 tweet_with_movie_df= tweet_with_movie_df.filter(tweet_with_movie_df['movie_name']!='null')
 
 tweet_with_movie_df.registerTempTable("tweetandmovie")
 
 
 
-
+#add date to the tweets based on when the tweet was generated. This is extracted from the 'created_at' column of the tweets
 def add_date_of_tweet(row):
 	dateonly = 'null'
 	if row.asDict()["created_at"] is not None:
@@ -113,24 +118,29 @@ def add_date_of_tweet(row):
 		dateonly= dateonly[0:10]
 	return(row.asDict()["created_at"], row.asDict()["text"], row.asDict()["movie_name"], dateonly)
 
-
+#rdd of my tweet query
 tweet_with_movie_and_date_rdd = tweet_with_movie_df.map(lambda row: add_date_of_tweet(row)).coalesce(40)
+
+#dataframe of tweet query
 tweetmoviedatedf=  sqlContext.createDataFrame(tweet_with_movie_and_date_rdd,['created_at', 'text', 'movie_name', 'dateonly'])
+
+#table with 'created_at', 'text' 'moviename' and 'dateonly' from tweets
 tweetmoviedatedf.registerTempTable("tweetmoviedate")
+
 
 maintweettable = tweetmoviedatedf.groupBy('dateonly','movie_name').count()
 maintweettable.registerTempTable("regtweet")
 NPdf.registerTempTable("regNPdf")
 
-
+#final table where tweets table was joined with TMDB table where moviename and data were matched
 final= sqlContext.sql("""SELECT * FROM regtweet JOIN regNPdf ON regtweet.movie_name = regNPdf.original_title AND regtweet.dateonly=regNPdf.date_added""")
 final.registerTempTable("final_table")
 final_collect = final.collect()
 
-print (type(final))
-final.printSchema()
-#final_table.show()
 
+final.printSchema()
+
+#create the cassandra table
 class correlationtable7(Model):
            moviename = columns.Text(primary_key = True)
            release_date = columns.Text()
@@ -141,14 +151,14 @@ class correlationtable7(Model):
            popularity = columns.Integer()
            voteaverage = columns.Integer()
            votecount = columns.Integer()
-   #retweetcount1 = columns.Text()
+  
            def __repr__(self):
                  return '%s %s %d %d %d %d %d %d' % (self.moviename, self.release_date, self.year, self.month, self.day, self.totaltweets, self.popularity, self.voteaverage, self.votecount) 
  
 connection.setup(['127.0.0.1'], "movietweets")
 sync_table(correlationtable7)
 
-
+#put all rows from final_collect to cassandra 
 for row in final_collect:
-        #print (row[0]) 
+         
 	correlationtable7.create(moviename=row[1].encode('ascii','ignore'), release_date=row[4].encode('ascii','ignore'), year=row[8], month=row[9], day=row[10], totaltweets=row[2], popularity = row[5], voteaverage=row[6], votecount=row[11])
